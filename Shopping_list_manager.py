4e0f0317eaba36df5b8149a74d2cb2fd4675d7ed
shubhmaheshwari3807-cmyk <shubhmaheshwari3807@gmail.com>
2026-07-01 00:27:06 +0530

    Add files via upload

---
+++ Shopping_list_manager.py
@@ -0,0 +1,10 @@
+items=[]
+items.append(input("Enter first item: "))
+items.append(input("Enter second item: "))
+items.append(input("Enter third item: "))
+items.append(input("Enter fourth item: "))
+items.append(input("Enter fifth item: "))
+
+print(items)
+
+print("Total items", len(items))
