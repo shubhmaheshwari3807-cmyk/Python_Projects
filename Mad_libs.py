32e22650a94a96b74c81273ad7cc65665ca47e9d
shubhmaheshwari3807-cmyk <shubhmaheshwari3807@gmail.com>
2026-06-30 22:54:59 +0530

    Add files via upload

---
+++ Mad_libs.py
@@ -0,0 +1,6 @@
+name=input("Give a name: ")
+animal=input("Give a animal: ")
+place=input("Give a place: ")
+
+statement="Once upon a time, " + name + " " + "met a " + animal + " " + "in " + place + "."
+print(statement)
