d19a0ed6e2c4a63821ebd4e32c4807f91781b30d
shubhmaheshwari3807-cmyk <shubhmaheshwari3807@gmail.com>
2026-07-01 00:46:23 +0530

    Add files via upload

---
+++ Tuple_info_finder.py
@@ -0,0 +1,4 @@
+cities=("delhi", "mumbai", "bareilly", "chandigarh", "chattisgarh", "kolkata", "jammu")
+city_name=input("Enter the city name(in lowercase): ")
+print("The city is loacted at index",cities.index(city_name))
+print("The city is", cities.count(city_name), "times in the tuple")
