295406629ace9394396231b9753ac96f10406591
Shubh Maheshwari <shubhmaheshwari3807@gmail.com>
2026-07-02 21:39:44 +0530

    Add files via upload

---
+++ word_counter.py
@@ -0,0 +1,16 @@
+word={}
+
+word1=input("Enter first word: ")
+count=word.get(word1,0)
+word[word1]=count +1
+
+word2=input("Enter second word: ")
+count=word.get(word2,0)
+word[word2]=count +1
+
+word3=input("Enter third word: ")
+count=word.get(word3,0)
+word[word3]=count +1
+
+print(word)
+
